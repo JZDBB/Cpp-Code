afe3c118aca71638b9825807b504d78c5c08ff2d
XuYining <oxuyining@gmail.com>
2020-03-23 18:46:37 +0800

    hash

--- workspace/Algorithms/Vector/main.cpp
+++ workspace/Algorithms/Vector/main.cpp
@@ -1,11 +1,11 @@
 #include<vector>
 using namespace std;
-#include"Solutions.h"
+#include"SumSolutions.h"
 
 
 
 int main() {
-	Solutions s = Solutions();
+	SumSolutions s = SumSolutions();
 	int a[6] = { -1,0,1,2,-1,-4 };
 	vector<int> nums(a, a+6);
 	vector<vector<int>> result = s.threeSum(nums);
